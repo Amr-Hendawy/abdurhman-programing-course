8535c62027c182d64fc5ff0afca2d2e1813ecb4e
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-06-25 16:35:08 +0300

    test

---
+++ week-2.js
@@ -0,0 +1,33 @@
+// Single Comment
+
+/*
+1
+2
+3
+4
+*/
+
+// TODO:: refactor this code
+
+// window.alert("Helli in js ")
+// document.write("<h1>Hello in document write</h1>")
+// console.log("Hello from js file")
+
+// Consoile Methods
+
+// console.error("Error Message")
+// console.info("Error Message")
+// console.warn("Error Message")
+
+// console.table(["Abdurhman", "Ahmed", "Ali"])
+
+
+console.log("Hello From %cJS %cFile", 
+    "color:#27ae60;background-color:#ecf0f1;font-weight:bold;font-size:40px;margin-top:20px;border-radius:6px;", "")
+
+// ECMA 
+
+var myName = "Amr";
+
+console.log("Hello " + myName);
+console.log(`Hello ${myName}`);
