7273f92101f947d8a78fd785346323370d8759f4
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-07-09 16:02:56 +0300

    solve task 4 week 1 and

--- week-1-tasks.js
+++ week-1-tasks.js
@@ -1,15 +1,34 @@
-window.onload = function(){
-    this.document.querySelector("h1").style.color = "blue";
-    this.document.querySelector("h1").style.fontSize = "80px";
-    this.document.querySelector("h1").style.fontWeight = "bold";
-    this.document.querySelector("h1").style.textAlign = "center";
-    this.document.querySelector("h1").style.fontFamily = "Arial";
-}
-console.log("%cElzero","color:red; 1px black; font-size:60px;")
-console.log("%cWeb","color:green; 1px black; font-size:60px;")
-console.log("%cSchool","background-color:blue; 1px black; font-size:60px;")
+// window.onload = function () {
+//   this.document.querySelector("h1").style.color = "blue";
+//   this.document.querySelector("h1").style.fontSize = "80px";
+//   this.document.querySelector("h1").style.fontWeight = "bold";
+//   this.document.querySelector("h1").style.textAlign = "center";
+//   this.document.querySelector("h1").style.fontFamily = "Arial";
+// };
+// console.log("%cElzero", "color:red; 1px black; font-size:60px;");
+// console.log("%cWeb", "color:green; 1px black; font-size:60px;");
+// console.log("%cSchool", "background-color:blue; 1px black; font-size:60px;");
 
-console.table(["elzero","ahmed","sameh","gamal","aya"])
+console.group("Group 1");
+console.log("Message One");
+console.log("Message Two");
+console.group("Child Group");
+console.log("Message One");
+console.log("Message Two");
+console.group("Child Group");
+console.group("Grand Child Group");
+console.log("Message One");
+console.log("Message Two");
+
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+
+console.group("Group 2");
+console.log("Message One");
+console.log("Message Two");
+console.groupEnd();
 
 /*
 console.log("Iam In Console");
--- week-2-tasks.js
+++ week-2-tasks.js
@@ -1,12 +1,18 @@
 let numberOne = 10;
 let numberTwo = 20;
-console.log(10 + "20")
-console.log(typeof (10 + "20"))
+console.log(10 + "20");
+console.log(typeof (10 + "20"));
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof ` ${numberOne}${numberTwo}`);
+console.log(numberOne + "\n" + numberTwo);
 
 console.log(elzero.innerHTML); // object
 console.log(typeof elzero); // object
+console.log();
 
 let a = 21;
 let b = 20;
 
-console.log("_"+a+"_"+b+a+"_"+b+a+"_"+b+a+"_"+b+"_"); // _21_2021_2021_2021_20_
+console.log("_" + a + "_" + b + a + "_" + b + a + "_" + b + a + "_" + b + "_"); // _21_2021_2021_2021_20_
+
+// console.log((amr.innerHTML = "Medhat"));
