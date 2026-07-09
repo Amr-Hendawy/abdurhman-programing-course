443812e0df33269077b0bf4e701c5aa86acb4a55
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-07-09 16:49:01 +0300

    completet number week

---
+++ week-4.js
@@ -0,0 +1,60 @@
+// Number
+//  Double Precision 10.00
+// Syntactic Sugar
+// console.log(1000000);
+// console.log(1_000_000);
+// console.log(1e6);
+// console.log(10 ** 6);
+// console.log(10 * 10 * 10 * 10 * 10 * 10);
+// console.log(1000000.0);
+
+// SNAKE_SCREAMING_CASE
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MAX_VALUE + 99999999e9);
+
+// two dots .. to call methods
+//  toString()
+//  toFixed()
+// parseInt()
+// parseFloat()
+// isInteger() [ES6]
+// isNaN() [ES6]
+
+// console.log((100.5).toString());
+// console.log((100.5).toString());
+// console.log((100.554555).toFixed(2));
+// console.log(Number("100 Amr"));
+// console.log(+"100 Amr");
+// console.log(parseInt("100 Amr"));
+// console.log(parseInt("Amr 100 Amr"));
+
+// console.log(parseInt(1050.3));
+// console.log(parseFloat(1050.3));
+// console.log(parseFloat("A 1050.3 Amr"));
+// console.log(parseFloat("1050.3 Amr"));
+
+// console.log(Number.isInteger("100"));
+// console.log(Number.isInteger(100));
+// console.log(Number.isInteger(100.66));
+
+// console.log(Number.isNaN(10));
+// console.log(Number.isNaN("Amr" / 10));
+
+// library VS framework
+// Math library
+//  round ceil flooor min max pow random trunc
+
+// console.log(Math.round(99.2));
+// console.log(Math.round(99.5));
+
+// console.log(Math.ceil(99.1));
+// console.log(Math.ceil(99.9));
+
+// console.log(Math.floor(99.0));
+// console.log(Math.floor(99.9));
+
+// console.log(Math.pow(2, 3));
+// console.log(Math.random());
+// console.log(Math.trunc(99.6));
+// console.log(Math.trunc(99.9));
