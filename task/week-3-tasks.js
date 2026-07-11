6b7243fb5d73c9ee40d742c4672b41ab636dd98f
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-07-11 10:57:42 +0300

    week 3 and week 4

---
+++ task/week-3-tasks.js
@@ -0,0 +1,38 @@
+// Replace ? With Arithmetic Operators
+//console.log(10 ? 20 ? 15 ? 3? 190 ? 10 ? 400); // 0
+
+
+let num = 3;
+
+// Solution One
+console.log(num * 2); // 6
+
+// Solution Two
+console.log(num + 3); // 6
+
+// Soultion Three
+console.log(num / 0.5); // 6
+
+// Soultion Four
+console.log(num + num + num - num); // 6
+
+// Solution Five
+console.log(num * (num - num + 2)); // 6
+
+// Solution Six
+console.log(); // 6
+
+
+let num2 = "10";
+
+// Solution One
+console.log( num2 * 2); // 20
+
+// Solution Two
+console.log(num2 * num2 -80); // 20
+
+// Solution Three
+console.log(num2 + num2); // 20
+
+// Solution Four
+console.log(num2 + num2 + num2 - num2); // 20
---
+++ task/week-4-tasks.js
@@ -0,0 +1,37 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(25e4 * 3); // 100000
+console.log(1e4); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+console.log(); // 100000
+
+console.log(Number.MIN_SAFE_INTEGER); // 9007199254740991
+console.log(Number.MAX_SAFE_INTEGER); // 16
+
+let myVar = "100.56789 Views";
+
+console.log((100.56789).toFixed(0)); // 100
+console.log((100.56789).toFixed(2)); // 100.57
+
+let num = 10;
+
+console.log(); // 2
+
+
+let flt = 10.4;
+
+console.log((flt).toString()); // 10
+console.log((flt).toFixed(0)); // 10
+console.log(); // 10
+console.log(); // 10
+console.log(); // 10
