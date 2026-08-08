dbb9ada268972b333a455cbf05fc1d1e7c736a5a
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-08-08 17:31:43 +0300

    completet loop part 1

---
+++ week-7.js
@@ -0,0 +1,29 @@
+// iterator
+
+// for (start; condition; steps) {
+//     // bloack of code
+// }
+
+// for (let i = 0; i < 15; i = i + 1) {
+
+// }
+// let myPals = [1, 2, 5, "Amr", "Osama", 6, "Abdurhman"];
+// // console.log(myPals[0]);
+// // console.log(myPals[1]);
+// // console.log(myPals[2]);
+// let onlyNames = [];
+// let onlyNums = [];
+// for (let i = 0; i < myPals.length; i++) {
+//   if (typeof myPals[i] === "string") {
+//     onlyNames.push(myPals[i]);
+//   }
+// }
+
+// for (let i = 0; i < myPals.length; i++) {
+//   if (typeof myPals[i] === "number") {
+//     onlyNums.push(myPals[i]);
+//   }
+// }
+
+// console.log(onlyNames);
+// console.log(onlyNums);
