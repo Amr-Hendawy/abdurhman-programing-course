40a65390aebb68ed49feea62e621a076c0f756e5
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-08-15 12:46:52 +0300

    47 - 53 tasks

---
+++ task/week-7-tasks.js
@@ -0,0 +1,77 @@
+// let start = 10;
+// let end = 100;
+// let exclude = 40;
+
+// for (i = start;i < end;i = i + start) {
+//     console.log(i);
+//     for (i = start; i == exclude;) {
+//         continue;
+//     }
+// }
+
+// let start = 10;
+// let end = 0;
+// let stop = 3;
+
+// for (i = start;i > 0;i--) {
+//     console.log(start)
+//     console.log(`0${i}`)
+//     for (i = start;i == 3;) {
+//         break;
+//     }
+// }
+
+// let start = 1;
+// let end = 6;
+// let breaker = 2;
+
+// for (i = start; i < end; i++) {
+//     console.log(i)
+//     console.log(`--${breaker}`)
+//     console.log(`--${breaker * breaker}`)
+// }
+
+// let index = 10;
+// let jump = 2;
+
+// for (i = index;i > jump * jump;i = i - jump) {
+//     console.log(i);
+// }
+
+// let friends = ["Ahmed", "Sayed", "Eman", "Mahmoud", "Ameer", "Osama", "Sameh"];
+// let letter = "a";
+// let names = []
+// for (i = 0; i < friends.length;i++) {
+//     if (friends.startsWith(letter)){
+//         continue;
+//     }
+//     else {
+//         names.push(friends(i))
+//     }
+//     console.log(`${names.length} => ${names}`)
+// }
+
+// let start = 0;
+// let swappedName = "elZerO";
+
+// for (i = start; i < swappedName;i++) {
+//     if (swappedName.indexOf(0) == ) {}
+// }
+
+// let start = 0;
+// let mix = [1, 2, 3, "A", "B", "C", 4];
+// onlyNums = [];
+
+// for (let i = 0; i < names.length; i++) {
+//     if (typeof names[i] === "number") {
+//         onlyNums.push(mix[i]);
+//     }
+//     else {
+//         continue;
+//     }
+// }
+
+// console.log(onlyNums)
+
+
+
