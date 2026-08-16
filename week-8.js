19dd7596a5af2a4054b38ddaed78f33162d7e2cc
Amr Hendawy <amr.fathy.ebraheem@gmail.com>
2026-08-16 18:32:48 +0300

    completet function week

--- week-8.js
+++ week-8.js
@@ -1,1 +1,29 @@
 // Functions
+// function sayHi(username) {
+//   console.log(`Hi ${username}`);
+// }
+
+// sayHi("Abdurhman");
+// sayHi("Amr");
+// sayHi("Ahmed");
+
+// function sayHi(username, age) {
+//   if (age < 20) {
+//     console.log(`App is not suitable for this age`);
+//   } else {
+//     console.log(`Hello ${username} your age is ${age}`);
+//   }
+// }
+// sayHi("Abdurhman", 21);
+// sayHi("Amr", 4);
+
+function getYears(start, end, exculde, stoppedYear) {
+  for (let i = start; i <= end; i++) {
+    if (i === exculde) continue;
+    if (i === stoppedYear) break;
+
+    console.log(i);
+  }
+}
+
+getYears(2005, 2026, 2020, 2021);
