3203eafded22a56da877a67877310deeb4229bc0
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-08-16 18:33:17 +0300

    function

--- app.js
+++ app.js
@@ -443,3 +443,32 @@ console.log(price)*/
 // console.log(index)
 
 // week8 - functions
+// function sayHi(username) {
+//     console.log(`Hi ${username}`);
+// }
+
+// sayHi("Abdulrahman")
+// sayHi("Amr")
+// sayHi("Ahmed")
+
+// function sayHi(username,age) {
+//     if (age <20) {
+//         console.log("App is not suitable for this age");
+//     } else {
+//         console.log(`Hello ${username} your age is ${age}`);
+//     }
+// }
+
+// sayHi("Abdulrahman",11)
+// sayHi("Amr",34)
+
+// function getYears(start,end,exclude,stoppedYear) {
+//     for (let i = start; i <= end; i++) {
+//         if (i === exclude) continue;
+//         if (i === stoppedYear) break;
+
+//         console.log(i);
+//     }
+// }
+
+// getYears(2005, 2026, 2020, 2021)
