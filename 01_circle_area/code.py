493dcd4aafece1bcc3943b7db89a792abf40d3f5
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-08-25 18:51:47 +0300

    week 1

---
+++ 01_circle_area/code.py
@@ -0,0 +1,7 @@
+import math
+
+radius = int(input("Enter your radius: "))
+
+area = radius * radius * math.pi
+
+print(area)
---
+++ script.js
@@ -0,0 +1,5 @@
+function calc(radius) {
+    let area = radius * radius * Math.PI
+    return area
+}
+console.log(calc(10))
