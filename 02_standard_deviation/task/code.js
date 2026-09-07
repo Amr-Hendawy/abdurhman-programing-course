f93ac53b44d9957abd081d53b21183559952e2cb
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-09-07 15:17:18 +0300

    tasks

---
+++ 02_standard_deviation/task/code.js
@@ -0,0 +1,23 @@
+x = []
+i = 0
+n = int(input("enter nums of nums do you have"))
+a = 0
+b = 0
+ave = a/n
+sd = 0
+total = 0
+if(i < x.length()){
+    i++;
+} else if (i > x.length()) {
+    total = total + x[i];
+    i++;
+}
+if(i < x.length()) {
+    total = total + x[i];
+    i++;
+    ave = total /n;
+    if(i < x.length()){
+        a = 9 + (xi - ave) * a;
+        b = a / n;
+    }
+}
