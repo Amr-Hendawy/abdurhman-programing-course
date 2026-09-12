e0ddd8770da4f941f35a919abffb4cec19bd945f
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-09-12 14:13:02 +0300

    update tasks.html to comment out week 9 tasks and add week 10 tasks

---
+++ task/week-10-tasks.js
@@ -0,0 +1,19 @@
+// let mix = [1, 2, 3, "E", 4, "l", "z", "e", "r", 5, "o"];
+// let mixMap = mix.map(function(e){
+//     return isNaN(parseInt(e)) ? true : false
+// })
+// console.log(mixMap)
+// Elzero
+
+// let myString = "EElllzzzzzzzeroo";
+// let filteredString = myString.filter(function(el){
+//     return myString
+// })
+// Elzero
+
+// let numsAndStrings = [1, 10, -10, -20, 5, "A", 3, "B", "C"];
+// let nums = numsAndStrings.filter(function(e){
+//     return !isNaN(parseInt(e))
+// })
+// console.log(nums)
+// [-1, -10, 10, 20, -5, -3]
