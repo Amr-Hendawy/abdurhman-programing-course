a662b75744ae4fbccc145c77d8474ec8e5843d25
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-09-16 11:54:08 +0300

    min and max

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,34 @@
+function min(arr) {
+  if (arr.length === 0) {
+    console.log("Array is empty");
+    return;
+  }
+
+  let min = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  console.log("min:", min);
+}
+function max(arr) {
+  if (arr.length === 0) {
+    console.log("Array is empty");
+    return;
+  }
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  console.log("Max:", max);
+}
+
 function calculate(op,...nums) {
     if(nums.length === 0){
         return "you need at least to type 2 numbers"
@@ -31,8 +62,11 @@ function calculate(op,...nums) {
         return result
     }
     if(op === "min") {
-        return 
+        min(nums)
+    }
+    if(op === "max") {
+        max(nums)
     }
 
 }
-console.log(calculate("-",4,2))
+console.log(calculate("max",4,2))
