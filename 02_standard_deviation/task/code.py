e157b81cf63bd4f0ebb66cb7c40a4fe436fb871e
ubadahmersal <abdulrahman@khalidalmaz.com>
2026-09-21 12:43:41 +0300

    task

--- 02_standard_deviation/task/code.py
+++ 02_standard_deviation/task/code.py
@@ -1,3 +1,24 @@
+import math
+
 x = []
-n = input("enter nums of nums do you have: ")
-i = a = b = ave = sd = total = 0
+sd = n = ave = a = b = i = 0
+
+
+n = int(input("How many Items?"))
+
+for i in range(n):
+    x.append(float(input("Give me item: ")))
+    ave += x[i]
+
+ave = ave / n
+
+print("Average:", ave)
+
+for j in range(n):
+    a += (x[j] - ave) ** 2
+    print(a)
+
+b = a / n
+sd = math.sqrt(b)
+
+print("Standard Deviation", sd)
